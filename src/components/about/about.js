f9f7dab5033e3beb0f046c461d05585a7e0b0e6a
Louay Hamada <loauy.hamada@gmail.com>
2018-04-29 01:05:20 +0300

    Move Testimonials from Home to About Component

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,6 +1,63 @@
 export default {
    name: 'AboutComponent',
    components: {},
-   data: () => ({}),
-   created() {}
+   data: () => ({
+    quotationIcon: '',
+    starIcon: '',
+    testimonials: {
+      title: 'من تجارب مجتمعنا',
+      carousel: [
+        {
+          img: 'saif.png',
+          name: 'منصور الجاسمي',
+          description: 'مصمم تطبيقات موبايل',
+          quote: 'Aliquam erat volutpat. Mauris ullamcorper, lectus in cursus ullamcorper, urna ligula scelerisque sapien, ac vehicula dolor libero eget tortor. Mauris et leo non eros vehicula tincidunt sit amet nec mi. Pellentesque viverra efficitur velit malesuada lobortis. Suspendisse suscipit venenatis dui ac pharetra. Nunc eu felis fermentum diam molestie dictum. Ut pretium laoreet tincidunt. Curabitur suscipit iaculis elit et lobortis.',
+          rating: 4
+        },
+        {
+          img: 'saif.png',
+          name: 'سيف الدين',
+          description: 'مطور واجهات ويب',
+          quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, libero in suscipit porta, lacus mauris volutpat neque, id finibus neque magna sit amet nulla. Fusce quis porta ante. Maecenas tincidunt diam et tortor pulvinar, eu tempor dui elementum. Morbi ultricies, eros ac tincidunt malesuada, nunc turpis convallis libero, vel dapibus enim quam sed mi. Cras consequat facilisis neque eu ullamcorper.',
+          rating: 5
+        },
+        {
+          img: 'saif.png',
+          name: 'سامر المصري',
+          description: 'مطور واجهات ويب',
+          quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus, libero in suscipit porta, lacus mauris volutpat neque, id finibus neque magna sit amet nulla. Fusce quis porta ante. Maecenas tincidunt diam et tortor pulvinar, eu tempor dui elementum. Morbi ultricies, eros ac tincidunt malesuada, nunc turpis convallis libero, vel dapibus enim quam sed mi. Cras consequat facilisis neque eu ullamcorper.',
+          rating: 3
+        },
+        {
+          img: 'saif.png',
+          name: 'بدر سيف',
+          description: 'مصمم تطبيقات موبايل',
+          quote: 'Aliquam erat volutpat. Mauris ullamcorper, lectus in cursus ullamcorper, urna ligula scelerisque sapien, ac vehicula dolor libero eget tortor. Mauris et leo non eros vehicula tincidunt sit amet nec mi. Pellentesque viverra efficitur velit malesuada lobortis. Suspendisse suscipit venenatis dui ac pharetra. Nunc eu felis fermentum diam molestie dictum. Ut pretium laoreet tincidunt. Curabitur suscipit iaculis elit et lobortis.',
+          rating: 2
+        }
+      ]
+    }
+   }),
+   created() {
+    this.$store.dispatch('getImgUrl', 'images/quotation.png').then(img => {
+      this.quotationIcon = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+
+    this.$store.dispatch('getImgUrl', 'images/star.png').then(img => {
+      this.starIcon = img
+    }).catch(error => {
+      throw new Error(error.message)
+    })
+
+    var root = this
+    root.testimonials.carousel.forEach(function(testimonial) {
+      root.$store.dispatch('getImgUrl', 'images/testimonials/' + testimonial.img).then(img => {
+        testimonial.img = img
+      }).catch(error => {
+        throw new Error(error.message)
+      })
+    })
+  }
 }
--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -1,5 +1,27 @@
 <template>
-<div class="about">
+<div id="about">
+  <v-container id="testimonials" fluid fill-height>
+    <v-layout fluid row justify-center wrap>
+      <v-flex text-xs-center>
+        <h2>{{ testimonials.title }}</h2>
+        <v-carousel cycle>
+          <v-carousel-item v-for="(testimonial,i) in testimonials.carousel" :key="i" transition="fade" reverse-transition="fade">
+            <img class="quotation" :src="quotationIcon" />
+            <div class="avatar" :style="{ backgroundImage: 'url(' + testimonial.img + ')' }" />
+            <div class="testimonial">
+              <h3 class="name">{{ testimonial.name }}<span class="description"> - {{ testimonial.description }}</span></h3>
+              <p class="quote">{{ testimonial.quote }}</p>
+              <div class="rating">
+                <span v-for="i in testimonial.rating" :key="i">
+                  <img :src="starIcon" />
+                </span>
+              </div>
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </div>
 </template>
 <script src="./about.js"></script>
