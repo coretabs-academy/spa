0d96260bda6d24a82a9131f780462a5ba8cee7c2
Louay Hamada <loauy.hamada@gmail.com>
2018-04-29 00:59:06 +0300

    Fix CSS Responsive Issues

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -17,35 +17,6 @@
         </v-flex>
       </v-layout>
   </v-container>
-  <v-container fluid fill-height>
-    <v-layout row justify-center>
-      <v-btn id="scroll" fab flat class="mt-5" @click="$vuetify.goTo('#testimonials',{duration: 0,offset: 0,easing: 'easeInOutCubic'})">
-        <img :src="scrollIcon" />
-      </v-btn>
-    </v-layout>
-  </v-container>
-  <v-container id="testimonials" fluid fill-height>
-    <v-layout fluid row justify-center wrap>
-      <v-flex text-xs-center>
-        <h2>{{ testimonials.title }}</h2>
-        <v-carousel cycle>
-          <v-carousel-item v-for="(testimonial,i) in testimonials.carousel" :key="i" transition="fade" reverse-transition="fade">
-            <img class="quotation" :src="quotationIcon" />
-            <div class="avatar" :style="{ backgroundImage: 'url(' + testimonial.img + ')' }" />
-            <div class="testimonial">
-              <h3 class="name">{{ testimonial.name }}<span class="description"> - {{ testimonial.description }}</span></h3>
-              <p class="quote">{{ testimonial.quote }}</p>
-              <div class="rating">
-                <span v-for="i in testimonial.rating" :key="i">
-                  <img :src="starIcon" />
-                </span>
-              </div>
-            </div>
-          </v-carousel-item>
-        </v-carousel>
-      </v-flex>
-    </v-layout>
-  </v-container>
 </div>
 </template>
 <script src="./home.js"></script>
