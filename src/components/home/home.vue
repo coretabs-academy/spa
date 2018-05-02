f034dec2d0b21f90cf2b729e989c04be67ec9e48
Louay Hamada <loauy.hamada@gmail.com>
2018-05-02 18:42:14 +0300

    Fix Data Binding System and Minor CSS Issues

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -10,12 +10,12 @@
           </video-player>
         </v-flex>
         <v-flex text-xs-right xs10 sm8 md5 id="introduction">
-            <h2 class="black--text">{{introduction.title}}</h2>
-            <p class="black--text mt-4 mb-0">{{introduction.p1}}</p>
-            <p class="black--text mt-4 mb-0">{{introduction.p2}}</p>
+            <h2 class="black--text" v-html="introduction.heading_text"></h2>
+            <p class="black--text mt-4 mb-0" v-html="introduction.description_text_1"></p>
+            <p class="black--text mt-4 mb-0" v-html="introduction.description_text_2"></p>
             <div id="introduction-buttons">
-              <v-btn id="yes" round dark class="mt-4 mb-0 mr-0 ml-3">{{introduction.button1}}</v-btn>
-              <v-btn id="no" round light class="mt-4 mb-0 mx-0">{{introduction.button2}}</v-btn>
+              <v-btn id="yes" round dark class="mt-4 mb-0 mr-0 ml-3" v-html="introduction.yes_btn_text"></v-btn>
+              <v-btn id="no" round light class="mt-4 mb-0 mx-0" v-html="introduction.no_btn_text"></v-btn>
             </div>
         </v-flex>
       </v-layout>
--- src/components/signup/signup.vue
+++ src/components/signup/signup.vue
@@ -5,15 +5,14 @@
         <v-flex id="container" xs8>
           <v-layout fluid row justify-center wrap fill-height>
             <v-flex xs10>
-              <h2 class="black--text">{{h2}}</h2>
-              <p class="black--text mt-2 mb-0">{{p}}</p>
+              <h2 class="black--text" v-html="heading_text"></h2>
+              <p class="black--text mt-2 mb-0" v-html="description_text"></p>
             </v-flex>
           </v-layout>
           <v-layout fluid row justify-center wrap>
             <v-flex id="avatars" xs0 sm0 md5>
               <v-layout row wrap justify-end>
-                  <v-flex class="avatar" xs4 v-for="i in 9" :key="`3${i}`">
-                  </v-flex>
+                  <v-flex class="avatar" xs4 v-for="i in 9" :key="`3${i}`"></v-flex>
               </v-layout>
             </v-flex>
             <v-flex id="form" xs10 sm10 md5>
@@ -21,24 +20,21 @@
                 <v-alert type="success" v-model="alert.success" v-text="alert.message"></v-alert>
                 <v-alert type="error" v-model="alert.error" v-text="alert.message"></v-alert>
 
-                <v-text-field label="اسمك الكامل" v-model="fullname" :class="[alert.success ? 'disabled' : '']" :rules="fnRules" :disabled="alert.success" required></v-text-field>
-                <v-text-field label="البريد الإلكتروني" v-model="email" :class="[alert.success ? 'disabled' : '']" :rules="emRules" :disabled="alert.success" required></v-text-field>
-                <v-text-field label="اسم المستخدم" v-model="username" :class="[alert.success ? 'disabled' : '']" :rules="unRules" :disabled="alert.success" required></v-text-field>
-                <v-text-field label="كلمة المرور" v-model="password" :class="[alert.success ? 'disabled' : '']" :rules="pwRules" :append-icon="pw ? 'visibility' : 'visibility_off'"
-          :append-icon-cb="() => (pw = !pw)"
-          :type="pw ? 'password' : 'text'" :disabled="alert.success" required></v-text-field>
+                <v-text-field :label="fullname_label" v-model="fullname" :class="[alert.success ? 'disabled' : '']" :rules="fnRules" :disabled="alert.success" required></v-text-field>
+                <v-text-field :label="email_label" v-model="email" :class="[alert.success ? 'disabled' : '']" :rules="emRules" :disabled="alert.success" required></v-text-field>
+                <v-text-field :label="username_label" v-model="username" :class="[alert.success ? 'disabled' : '']" :rules="unRules" :disabled="alert.success" required></v-text-field>
+                <v-text-field :label="password_label" v-model="password" :class="[alert.success ? 'disabled' : '']" :rules="pwRules"
+                              :append-icon="pw ? 'visibility' : 'visibility_off'" :append-icon-cb="() => (pw = !pw)" :type="pw ? 'password' : 'text'" :disabled="alert.success" required></v-text-field>
               </v-form>
             </v-flex>
           </v-layout>
           <v-layout fluid row justify-center wrap>
             <v-flex xs10 sm10 md7>
-              <p id="tos" v-html="tos" class="black--text mt-1 mb-0">
+              <p id="tos" v-html="tos_text" class="black--text mt-1 mb-0">
               </p>
             </v-flex>
             <v-flex xs10 sm10 md3>
-              <v-btn round id="submit" @click="submit" v-show="!alert.success" :disabled="valid < 4">
-                إنشاء الحساب
-              </v-btn>
+              <v-btn round id="submit" @click="submit" v-show="!alert.success" v-text="submit_btn_text" :disabled="valid < 4"></v-btn>
             </v-flex>
           </v-layout>
         </v-flex>
