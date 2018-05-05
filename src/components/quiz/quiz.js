e286ebda5ad74481d6d22435bb34833bcf3be11a
Louay Hamada <loauy.hamada@gmail.com>
2018-05-05 18:15:25 +0300

    Apply Check Answers Logic

--- src/components/quiz/quiz.js
+++ src/components/quiz/quiz.js
@@ -1,41 +1,81 @@
 export default {
-    data() {
-        return {
-            successTxt: 'أحسنت اللإجابة صحيحة',
-            failTxt: 'أحسنت المحاولة!',
-            retryTxt: 'أعد المحاولة',
-            confirmTxt: 'تأكيد الإجابة',
-            nextQs: 'السؤال التالي',
-            e1: 0,
-            items: [
-                {
-                    'question': 'bla bla bla',
-                    'answers': {
-                        '1': 'beboba',
-                        '2': 'aeaoaa'
-                    },
-                    'correct': ['1'],
-                    'hint': 'koko kaka'
-                },
-                {
-                    'question': 'ola ola ola',
-                    'answers': {
-                        '1': 'beboba',
-                        '2': 'aeaoaa'
-                    },
-                    'correct': ['2'],
-                    'hint': 'tiki taka'
-                },
-                {
-                    'question': 'pla pla pla',
-                    'answers': {
-                        '1': 'beboba',
-                        '2': 'aeaoaa'
-                    },
-                    'correct': ['1', '2'],
-                    'hint': 'foo boo'
-                }
-            ]
+  data() {
+    return {
+      e1: 0,
+      heading_text: 'اختر الإجابة الصحيحة',
+      result: '',
+      results_texts: {
+        success: 'أحسنت الإجابة صحيحة',
+        fail: 'أحسنت المحاولة! ',
+        hint: 'هل تريد الحصول على تلميح ؟'
+      },
+      buttons_texts: {
+        confirm: 'تأكيد الإجابة',
+        retry: 'أعد المحاولة',
+        dismiss: 'تجاهل السؤال',
+        next: 'السؤال التالي'
+      },
+      questions: [
+        {
+          'text': 'bla bla bla',
+          'answers': {
+            '1': 'beboba',
+            '2': 'aeaoaa'
+          },
+          'correct': ['1'],
+          'hint': 'koko kaka',
+          choose: []
+        },
+        {
+          'text': 'ola ola ola',
+          'answers': {
+            '1': 'beboba',
+            '2': 'aeaoaa'
+          },
+          'correct': ['2'],
+          'hint': 'tiki taka',
+          choose: []
+        },
+        {
+          'text': 'pla pla pla',
+          'answers': {
+            '1': 'beboba',
+            '2': 'aeaoaa'
+          },
+          'correct': ['1', '2'],
+          'hint': 'foo boo',
+          choose: []
         }
+      ]
     }
+  },
+  methods: {
+    chooseAnswer(question, answer) {
+      if (question.correct.length === 1) {
+        question.choose = []
+        if (!question.choose.includes(answer)) {
+          question.choose.push(answer)
+        }
+        this.checkAnswers(question)
+      } else {
+        if (question.choose.includes(answer)) {
+          question.choose.splice(question.choose.indexOf(answer), 1)
+        } else {
+          question.choose.push(answer)
+        }
+      }
+    },
+    checkAnswers(question) {
+      question.choose.sort()
+      if (question.choose.toString() === question.correct.toString()) {
+        this.result = this.results_texts.success
+      } else {
+        this.result = this.results_texts.fail
+      }
+    },
+    goNext() {
+      this.e1 += 1
+      this.result = ''
+    }
+  }
 }
--- src/components/quiz/quiz.vue
+++ src/components/quiz/quiz.vue
@@ -1,53 +1,55 @@
 <template>
-    <v-layout row wrap class="quizz-layout">
-        <v-flex xs12 lg8 offset-lg2 md8 offset-md2>
-    <v-stepper v-model="e1" class="quizz-stepper">
+  <v-layout row wrap class="quizz-layout">
+    <v-flex xs12 lg8 offset-lg2 md8 offset-md2>
+      <v-stepper v-model="e1" class="quizz-stepper">
         <v-stepper-header>
-            <template v-for="(item, index) in items">
-          <v-stepper-step  :step="index+1" :key="index" :complete="e1 > index+1"></v-stepper-step>
-          <v-divider v-if="index + 1 < items.length" :key="`divider-${index}`"></v-divider>
-            </template>
+          <template v-for="(question, qIndex) in questions">
+            <v-stepper-step  :step="qIndex+1" :key="qIndex" :complete="e1 > qIndex+1"></v-stepper-step>
+            <v-divider v-if="qIndex + 1 < questions.length" :key="`divider-${qIndex}`"></v-divider>
+          </template>
         </v-stepper-header>
         <v-stepper-items>
-            <v-card   height="20px" flat>
-              <p class="question-num">السؤال {{e1}} من {{items.length}} : لإختر اللإجابة الصحيحة</p>
-            </v-card>
-            <template v-for="(item, index) in items">
-          <v-stepper-content :step="index+1" :key="index">
-            <h3 class="question-content">{{item.question}} ؟</h3>
-            <v-card color="grey lighten-1" flat>
-              <!-- test -->
-              <v-list three-line subheader>
-                  <template v-for="(answer, indexx) in item.answers">
-                      <v-divider></v-divider>
-                  <v-list-tile href="javascript:;" :key="indexx" class=""> 
-                    <v-list-tile-action>
-                      <input type="checkbox" v-model="choose" :value="answer"/>
-                      <span class="checkbox_cont"><span v-show="true" class="checkbox_inner"></span></span>
-                    </v-list-tile-action>
-                    <v-list-tile-content @click="">
-                      <v-list-tile-title>{{answer}}</v-list-tile-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
+          <v-card v-show="e1 <= questions.length" flat>
+            <p class="question-num py-0">السؤال {{e1}} من {{questions.length}} : {{heading_text}}</p>
+          </v-card>
+          <template v-for="(question, qIndex) in questions">
+            <v-stepper-content :step="qIndex + 1" :key="qIndex">
+              <h3 class="question-content">{{question.text}}</h3>
+              <v-card color="grey lighten-1" flat>
+                <!-- one-correct answer -->
+                <v-list three-line subheader>
+                  <template v-for="(answer, aIndex) in question.answers">
+                    <v-divider></v-divider>
+                    <v-list-tile @click="chooseAnswer(question, aIndex)" :key="aIndex">
+                      <v-list-tile-action>
+                        <input type="checkbox" v-model="question.choose" :value="answer" />
+                        <span class="checkbox_cont">
+                          <span v-show="question.choose.includes(aIndex)" class="checkbox_inner"></span>
+                        </span>
+                      </v-list-tile-action>
+                      <v-list-tile-content>
+                        <v-list-tile-title>{{answer}}</v-list-tile-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
                   </template>
-                  <v-divider :key="`divider-${index}`"></v-divider>
+                  <v-divider :key="`divider-${qIndex}`"></v-divider>
                 </v-list>
-              <!-- test -->
-            </v-card>
-            <!-- <v-btn color="primary" @click.native="e1 = index +2">test</v-btn> -->
-          </v-stepper-content>
+              </v-card>
+              <!-- <v-btn color="primary" @click.native="e1 = index +2">test</v-btn> -->
+            </v-stepper-content>
           </template>
-          <v-card    class="btns-control" flat>
-            <v-btn v-show="true" class="r-btn r-white " flat @click.native="e1 += 1">تخطي الإجابة</v-btn> <v-btn class="r-btn" flat>{{retryTxt}}</v-btn>
-            <span  class="result-container">
-              <span v-show="true" :class="`result err`">{{failTxt}}</span> 
-              <span v-show="true" class="hint">هل تريد الحصول على تلميح ؟ <span class="circle"><v-icon color="white" size="large">star</v-icon></span></span>
+          <v-card class="btns-control" flat>
+            <v-btn v-show="e1 < questions.length" class="r-btn" flat @click="goNext" :disabled="!result || result === results_texts.fail">{{buttons_texts.next}}</v-btn>
+            <v-btn v-if="e1 > 0" v-show="questions[e1 - 1].correct.length > 1" class="r-btn" flat @click="checkAnswers(questions[e1 - 1])">{{buttons_texts.confirm}}</v-btn>
+            <span class="result-container">
+              <span v-show="result" :class="['result', result === results_texts.fail ? 'err' : '']">{{result}}</span>
+              <span v-show="result === results_texts.fail" class="hint">{{results_texts.hint}} <span class="circle"><v-icon color="white" size="small">star</v-icon></span></span>
             </span>
-            </v-card>
+          </v-card>
         </v-stepper-items>
       </v-stepper>
-  </v-flex>
-    </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 <script src="./quiz.js"></script>
 <style src="./quiz.scss" lang="scss"></style>
